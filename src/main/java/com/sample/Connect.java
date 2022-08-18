e003f27770563902a95bab1e7f7906f08e51caba
Ethan Yin <yinssh1005@gmail.com>
2022-08-18 16:38:39 +0800

    Initial commit

---
+++ src/main/java/com/sample/Connect.java
@@ -0,0 +1,46 @@
+package com.sample;
+
+import java.sql.*;
+
+public class Connect {
+
+    private static final String JDBC_URL_CONST = "jdbc:sqlite:D:\\idea_projects\\connect-to-sqlite-manually\\src\\main\\resources\\new_db";
+    private static Statement statement;
+    private static Connection connection;
+
+    /**
+     * Connect to a sample database
+     */
+    public static void connect() {
+        try {
+
+            // create a connection to the database
+            Class.forName("org.sqlite.JDBC");
+            connection = DriverManager.getConnection(JDBC_URL_CONST);
+
+            System.out.println("Connection to SQLite has been established.");
+
+            String sql = "SELECT * FROM Users";
+
+            statement = connection.createStatement();
+            readRs(sql);
+        } catch (SQLException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    private static void readRs(String sql) throws SQLException {
+        ResultSet rs = statement.executeQuery(sql);
+
+        // loop through the result set
+        while (rs.next()) {
+            System.out.println(rs.getInt("id") + "\t" +
+                    rs.getString("name"));
+        }
+    }
+
+    public static void main(String[] args) {
+        connect();
+    }
+}
